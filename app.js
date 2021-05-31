b71d08313b4725b8a67a4139d3d08733567339bd
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-05-31 22:28:09 +0200

    Complete navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.render('home');

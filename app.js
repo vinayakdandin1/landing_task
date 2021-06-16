cd2a38db2a36726c582ca2c1f6ac99acc1dc005e
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-06-16 16:41:16 +0200

    updated port details to uplaod to heroku

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.get('/aidkit', function (req, res) {
     res.render('aidkit');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Server started on port 3000');
+let port = process.env.PORT;
+
+if(port == null || port == "") {
+    port = 3000
+}
+
+app.listen(port, function () {
+    console.log('Server has started sucessfully');
 });
